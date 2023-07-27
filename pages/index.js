d0218c7672c7601c7f99684e795131902faaf4df
Arash Zarei <arashzarei.js@gmail.com>
2023-07-27 13:13:32 +0330

    conver HomePage to SSG Page

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,18 @@
 import HomePage from "../components/templates/HomePage";
 
 
-const Home = () => {
-  return <HomePage />
+const Home = ({ movies }) => {
+  return <HomePage data={movies} />
 }
 
 export default Home;
+
+export async function getStaticProps() {
+  const res = await fetch("http://localhost:3030/movies");
+  const data = await res.json()
+  const limitData = data.slice(0, 9)
+
+  return {
+    props: { movies: limitData }
+  }
+}
