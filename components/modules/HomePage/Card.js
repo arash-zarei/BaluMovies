b92c25edf5ef0086c6c0804a17946ec735c3e3b3
Arash Zarei <arashzarei.js@gmail.com>
2023-07-27 13:21:11 +0330

    create card movie component and get daynamic data

---
+++ components/modules/HomePage/Card.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+// Icons
+import * as Unicons from "@iconscout/react-unicons";
+
+const Card = ({ data }) => {
+
+  const {  en_name, fa_name, genre, score, id } = data
+
+  return (
+    <div className="rounded-xl mt-8 relative text-white w-fit">
+      <img src={`/images/${id}.jpg`} alt={fa_name} className="rounded-xl" />
+      <div className="detailsCard">
+        <div>
+          <p className="text-left">{en_name}</p>
+          <p>{fa_name}</p>
+        </div>
+        <div className="flex justify-between items-center flex-col gap-10">
+          <div className="w-full flex justify-between items-center">
+            <p>{genre}</p>
+            <div className="flex items-center gap-1">
+              <img src="/images/imdb.png" alt="imdb" className="w-7" />
+              <p>{score}</p>
+            </div>
+          </div>
+          <div className="w-full flex justify-between items-center gap-2">
+            <button className="w-full py-1 bg-primary hover:bg-yellow-400 text-black text-center rounded-md">
+              تماشا
+            </button>
+            <span className="p-1 rounded-md bg-gray-600 cursor-pointer">
+              <Unicons.UilPlus />
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- components/templates/HomePage.js
+++ components/templates/HomePage.js
@@ -1,17 +1,29 @@
 import React from "react";
+import Link from "next/link";
 
 // Components
 import Slider from "../modules/HomePage/Slider";
+import Card from "../modules/HomePage/Card";
 
-const HomePage = () => {
+const HomePage = ({ data }) => {
   return (
     <div className="w-full px-4">
       <Slider />
-      {/* card movie */}
-      <div className="w-full grid grid-cols-1">
 
+      {/* card movie */}
+      <div className="w-full grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-3">
+        {data.map((movie) => (
+          <Card key={movie.id} data={movie} />
+        ))}
       </div>
       {/* end card movie */}
+
+      <Link
+        href="/movies"
+        className="w-full block py-1 my-10 text-center bg-primary hover:bg-yellow-400 rounded-md"
+      >
+        فیلم های بیشتر
+      </Link>
     </div>
   );
 };
