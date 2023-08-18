88b6fe6e692e986964b9c7858f22abf75ea1e361
Arash Zarei <arashzarei.js@gmail.com>
2023-08-18 13:49:46 +0330

    get data Movies Page from mongoDB

--- pages/movies/index.js
+++ pages/movies/index.js
@@ -1,17 +1,25 @@
+import Movies from "models/Movies";
+import connectDB from "utils/connectDB";
+
 // Components
 import MoviesPage from "@/templates/MoviesPage";
 
 const index = ({ movies }) => {
-    return <MoviesPage data={movies} />
+  return <MoviesPage data={movies} />;
 };
 
 export default index;
 
 export async function getStaticProps() {
-    const res = await fetch("https://balu-movies-api.vercel.app/movies");
-    const data = await res.json()
-  
+  try {
+    await connectDB();
+    const data = await Movies.find();
     return {
-      props: { movies: data }
-    }
+      props: { movies: JSON.parse(JSON.stringify(data)) },
+    };
+  } catch (error) {
+    return {
+      notFound: true,
+    };
   }
+}
