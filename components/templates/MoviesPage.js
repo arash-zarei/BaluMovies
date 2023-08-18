40b4dc2345caf03c736c456cccbd12fed59543db
Arash Zarei <arashzarei.js@gmail.com>
2023-08-18 14:49:14 +0330

    edit key value

--- components/templates/MoviesPage.js
+++ components/templates/MoviesPage.js
@@ -7,7 +7,7 @@ const MoviesPage = ({ data }) => {
     return (
         <div className="w-full grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 gap-3 mb-8 px-4">
             {data.map(movie => (
-                <Card key={movie.id} data={movie} />
+                <Card key={movie._id} data={movie} />
             ))}
         </div>
     );
