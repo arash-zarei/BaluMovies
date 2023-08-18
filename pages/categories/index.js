fce104455656e803ca58794f8fc76020565a1e15
Arash Zarei <arashzarei.js@gmail.com>
2023-08-18 14:00:49 +0330

    get data's categories page from mongoDB

--- pages/categories/index.js
+++ pages/categories/index.js
@@ -1,3 +1,6 @@
+import Movies from "models/Movies";
+import connectDB from "utils/connectDB";
+
 // Components
 import CategoriesPage from "@/templates/CategoriesPage";
 
@@ -11,37 +14,47 @@ export async function getServerSideProps(context) {
   const {
     query: { score, year },
   } = context;
-  const res = await fetch("https://balu-movies-api.vercel.app/movies");
-  const data = await res.json();
+  try {
+    await connectDB();
+    const data = await Movies.find();
 
-  const FiltredData = data.filter((movie) => {
-    // Filtred By Year
-    const yearsResult = movie.year_score.filter(
-      (years) => years.year && years.year === year
-    );
+    const FiltredData = data.filter((movie) => {
+      // Filtred By Year
+      const yearsResult = movie.year_score.filter(
+        (years) => years.year && years.year === year
+      );
 
-    // Filtred By Score
-    const scoreResult = movie.year_score.filter((scores) => {
-      if (score === "2-5" && scores.score >= "2" && scores.score <= "5") {
+      // Filtred By Score
+      const scoreResult = movie.year_score.filter((scores) => {
+        if (score === "2-5" && scores.score >= "2" && scores.score <= "5") {
+          return movie;
+        } else if (
+          score === "5-7" &&
+          scores.score > "5" &&
+          scores.score <= "7"
+        ) {
+          return movie;
+        } else if (score === "7" && scores.score > "7") {
+          return movie;
+        }
+      });
+
+      if (score && year && yearsResult.length && scoreResult.length) {
         return movie;
-      } else if (score === "5-7" && scores.score > "5" && scores.score <= "7") {
+      } else if (!score && year && yearsResult.length) {
         return movie;
-      } else if (score === "7" && scores.score > "7") {
+      } else if (score && !year && scoreResult.length) {
+        console.log(movie);
         return movie;
       }
     });
 
-    if (score && year && yearsResult.length && scoreResult.length) {
-      return movie;
-    } else if (!score && year && yearsResult.length) {
-      return movie;
-    } else if (score && !year && scoreResult.length) {
-      console.log(movie);
-      return movie;
-    }
-  });
-
-  return {
-    props: { movie: FiltredData },
-  };
+    return {
+      props: { movie: JSON.parse(JSON.stringify(FiltredData)) },
+    };
+  } catch (error) {
+    return {
+      notFound: true,
+    };
+  }
 }
