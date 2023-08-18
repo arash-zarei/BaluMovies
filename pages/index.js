23801d6b6a33d4826ce0fbfde3e6de52d767d548
Arash Zarei <arashzarei.js@gmail.com>
2023-08-18 13:44:40 +0330

    get data's Home Page from mongoDB

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,27 @@
+import connectDB from "utils/connectDB";
+import Movies from "models/Movies";
+
 // Componets
 import HomePage from "@/templates/HomePage";
 
-
 const Home = ({ movies }) => {
-  return <HomePage data={movies} />
-}
+  return <HomePage data={movies} />;
+};
 
 export default Home;
 
 export async function getStaticProps() {
-  const res = await fetch("https://balu-movies-api.vercel.app/movies");
-  const data = await res.json()
-  const limitData = data.slice(0, 9)
+  try {
+    await connectDB();
+    const data = await Movies.find();
+    const limitMovies = data.slice(0, 11);
 
-  return {
-    props: { movies: limitData }
+    return {
+      props: { movies: JSON.parse(JSON.stringify(limitMovies)) },
+    };
+  } catch (error) {
+    return {
+      notFound: true,
+    };
   }
 }
