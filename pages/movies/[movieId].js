f6b1a686545affbeae4982b8c03a0d74ff97ed59
Arash Zarei <arashzarei.js@gmail.com>
2023-08-18 13:55:45 +0330

    get data's Movie Page from mongoDB

--- pages/movies/[movieId].js
+++ pages/movies/[movieId].js
@@ -1,53 +1,66 @@
-import { useRouter } from 'next/router';
-import React from 'react';
-import Head from 'next/head';
+import { useRouter } from "next/router";
+import React from "react";
+import Head from "next/head";
+
+import connectDB from "utils/connectDB";
 
 // Components
-import MoviePage from '@/templates/MoviePage';
-import Loading from '@/modules/MoviePage/Loading';
+import MoviePage from "@/templates/MoviePage";
+import Loading from "@/modules/MoviePage/Loading";
+import Movies from "models/Movies";
 
 const MovieDetails = ({ movie }) => {
+  const params = useRouter();
 
-    const params = useRouter()
+  if (params.isFallback) {
+    return <Loading />;
+  }
 
-    if(params.isFallback) {
-        return <Loading />
-    }
-
-    return (
-        
-        <>
-        <Head>
-            <title>{movie.fa_name}</title>
-            <meta name='description' content={movie.synopsis} />
-        </Head>
-        <MoviePage data={movie} />
-        </>
-    )
+  return (
+    <>
+      <Head>
+        <title>{movie.fa_name}</title>
+        <meta name="description" content={movie.synopsis} />
+      </Head>
+      <MoviePage data={movie} />
+    </>
+  );
 };
 
 export default MovieDetails;
 
-export async function getStaticPaths (){
-    const res = await fetch("https://balu-movies-api.vercel.app/movies");
-    const data = await res.json()
-    const limitData = data.slice(0, 15)
-    const paths = limitData.map(item => ({
-        params: { movieId: item.id.toString() }
-    }))
-
+export async function getStaticPaths() {
+  try {
+    await connectDB();
+    const data = await Movies.find();
+    const limitData = data.slice(0, 15);
+    const paths = limitData.map((item) => ({
+      params: { movieId: item._id.toString() },
+    }));
     return {
-        paths,
-        fallback: true,
-    }
+      paths,
+      fallback: true,
+    };
+  } catch (error) {
+    return {
+      notFound: true,
+    };
+  }
 }
 
 export async function getStaticProps(context) {
-    const { params: { movieId } } = context;
-    const res = await fetch(`https://balu-movies-api.vercel.app/movies/${movieId}`);
-    const data = await res.json();
-
+  const {
+    params: { movieId },
+  } = context;
+  try {
+    await connectDB();
+    const data = await Movies.findOne({ _id: movieId });
     return {
-        props: { movie: data }
-    }
+      props: { movie: JSON.parse(JSON.stringify(data)) },
+    };
+  } catch (error) {
+    return {
+      notFound: true,
+    };
+  }
 }
